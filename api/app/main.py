d433db0519fd443d37d756bd7b06e91c9f4dded5
Shayne Meyer <shayne.meyer@gmail.com>
2025-02-09 14:34:18 -0800

    added test route to app

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from app.dependencies.sheets import get_google_sheets_service
+from app.services.google_sheets import GoogleSheetsService
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "everything looks good!"}
+
+@app.get("/test")
+def read_root(
+    service: GoogleSheetsService = Depends(get_google_sheets_service)
+):
+    return {"message": repr(service.sheets_service)}
